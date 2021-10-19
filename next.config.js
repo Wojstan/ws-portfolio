22850564e575e06adfdf7551a05b12d8dda5fbe1
Wojstan <wpstaniszewski@gmail.com>
2021-10-19 11:59:18 +0200

    Modify api path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
   reactStrictMode: true,
   env: {
     ROOT:
-      process.env.NODE_ENV === "production" ? "" : `http://localhost:3000/api`,
+      process.env.NODE_ENV === "production"
+        ? "http://localhost:3000/api"
+        : `http://localhost:3000/api`,
   },
 };
